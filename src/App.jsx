d3b11756d464fadb4d9dacf20f0857d079da325e
unknown <karthikeya3762@gmail.com>
2023-12-06 19:23:56 +0530

    event handlingt onClick event handling using arraow function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Note from './components/note'
 function App({notse}) {
 
- function handleSaveClick(event){
-  console.log(event);
-  event.preventDefault();
-  console.log("Butto clicked");
- }
+//  function handleSaveClick(event){
+//   console.log(event);
+//   event.preventDefault();
+//   console.log("Butto clicked");
+//  }
   
   return (
     <div>
@@ -20,7 +20,10 @@ function App({notse}) {
 
     <form >
       <input/>
-      <button onClick={handleSaveClick}>Save note</button>
+      <button onClick={(event)=>{
+        event.preventDefault();
+        console.log("sve button clicked function inside the onclick event")
+      }}>Save note</button>
     </form>
     </div>
     
