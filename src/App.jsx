182a9a53b114b23658afed7a7fea511b3b49e473
unknown <karthikeya3762@gmail.com>
2023-12-06 11:28:30 +0530

    create a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
-
-function Hello(props) {
-  console.log(props);
-  return(
-    <p>Hello{props.userName}</p>
-  )
-}
-
-
-
-
 function App(){
-
-let user1="Karthi";
-let user2="Karthikeyan M";
-
-
-  return(
-    <div>
-      <h1>Gretings</h1>
-      <Hello userName={user1}/>
-       <Hello userName={user2}/>
-    </div>
-  )
+  return (<div>
+    <p>Hello world</p>
+  </div>)
 }
 export default App;
