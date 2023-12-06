1572abf7a9aba6764cfde80ee60f23905426e4d8
unknown <karthikeya3762@gmail.com>
2023-12-06 11:37:48 +0530

    Component with multiple html element

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-const App1=()=>{
-  return(
-    <h1>Heading</h1>
-  )
-}
-
-
-
-
 function App(){
-  return (
-    <>
-    <p>
-      Hello world
-    </p>
-    <App1/>
-    </>
+  return(
+
+    <div>
+    <h1>App Component</h1>
+    <p>Hello world</p>
+
+    </div>
   )
 }
-
-
-
 export default App;
-
